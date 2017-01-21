801b7dd319797e8564ac5d331d42e6b856f5268e
jianqiu <wangjq@cn.ibm.com>
2017-01-21 15:26:47 +0800

    debug

--- vendor/github.com/maximilien/softlayer-go/data_types/softlayer_network_vlan.go
+++ vendor/github.com/maximilien/softlayer-go/data_types/softlayer_network_vlan.go
@@ -5,14 +5,14 @@ import (
 )
 
 type SoftLayer_Network_Vlan struct {
-	AccountId       int                       `json:"accountId"`
-	Id              int                       `json:"id"`
+	AccountId       int                       `json:"accountId,omitempty"`
+	Id              int                       `json:"id,omitempty"`
 	ModifyDate      *time.Time                `json:"modifyDate,omitempty"`
-	Name            string                    `json:"name"`
-	NetworkVrfId    int                       `json:"networkVrfId"`
-	Note            string                    `json:"note"`
-	PrimarySubnetId int                       `json:"primarySubnetId"`
-	VlanNumber      int                       `json:"vlanNumber"`
+	Name            string                    `json:"name,omitempty"`
+	NetworkVrfId    int                       `json:"networkVrfId,omitempty"`
+	Note            string                    `json:"note,omitempty"`
+	PrimarySubnetId int                       `json:"primarySubnetId,omitempty"`
+	VlanNumber      int                       `json:"vlanNumber,omitempty"`
 	NetworkSpace    string                    `json:"networkSpace"`
-	Subnets         SoftLayer_Network_Subnets `json:"subnets"`
+	Subnets         SoftLayer_Network_Subnets `json:"subnets,omitempty"`
 }
